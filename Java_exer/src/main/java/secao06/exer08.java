610f405e64a9f4dcd235c2b61ba7dc7579b12047
usuario <iwatalucas@gmail.com>
2026-03-25 17:40:13 -0300

    secao06 exer 8 e 9

--- Java_exer/src/main/java/secao06/exer08.java
+++ Java_exer/src/main/java/secao06/exer08.java
@@ -3,8 +3,33 @@ package secao06;
 //Faça um algoritmo que leia um numero inteiro
 // e mostre uma mensagem indicando se este numero é par ou impar, e se é positivo ou negativo
 
+import java.util.Scanner;
+
 public class exer08 {
     public static void main(String[] args) {
 
+        //variaveis
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        //entrada
+        System.out.print("Digite um numero: ");
+        num = sc.nextInt();
+
+        //processamento
+        if (num % 2 == 0) {
+            if (num > 0) {
+                System.out.printf("O número %d é par e positivo./n", num);
+            }else {
+                System.out.printf("O número %d é par e negativo./n", num);
+            }
+        }else {
+            if (num > 0) {
+                System.out.printf("O número %d é ímpar e positivo./n", num);
+            }else {
+                System.out.printf("O número %d é impar e negativo./n", num);
+            }
+        }
+        sc.close();
     }
 }
